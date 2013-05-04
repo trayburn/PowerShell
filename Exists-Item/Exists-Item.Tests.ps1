2f5caa0565a46fb79d9f6b64a9d72559cb5ec8b5
Tim Rayburn <tim@timrayburn.net>
2013-05-03 20:38:43 -0500

    Exists-Item tests and script

---
+++ Exists-Item/Exists-Item.Tests.ps1
@@ -0,0 +1,75 @@
+$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+    $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.", ".")
+    . "$here\$sut"
+
+    Describe "Exists-Item when directory exists" {
+    		Context "when directory exists" {
+    		
+	    		Setup -Dir "DirectoryThatExists"
+					$testDir = "TestDrive:\DirectoryThatExists"
+					
+					$script:testY = ""
+					$script:testN = ""
+					$script:testA = ""
+					
+	        Exists-Item $testDir `
+	        	-Yes { $script:testY = $_  } `
+	        	-No { $script:testN = $_ } `
+	        	-Always { $script:testA = $_ }
+
+	        It "executes -Yes" {
+	            $script:testY | should not benullorempty
+	        }
+	        It 'passes item as $_ to -Yes' {
+	        		$script:testY | should be $testDir
+	        }
+	  			It "does not execute -No" {
+	            $script:testN | should benullorempty
+	        }
+	        It "executes -Always" {
+	            $script:testA | should not benullorempty
+	        }
+	        It 'passes item as $_ to -Always' {
+							$script:testA | should be $testDir
+	        }
+        }
+        
+        
+        Context "when directory does not exists" {
+    		
+	    		$testDir = "TestDrive:\DirectoryThatDoesNotExists"
+
+					$script:testY = ""
+					$script:testN = ""
+					$script:testNValue = ""
+					$script:testA = ""
+					$script:testAValue = ""
+					
+	        Exists-Item $testDir `
+	        	-Yes { $script:testY = $true  } `
+	        	-No { 
+	        		$script:testN = $true 
+	        		$script:testNValue = $_
+	        	} `
+	        	-Always { 
+	        		$script:testA = $true 
+	        		$script:testAValue = $_
+	        	}
+
+	        It "does not execute -Yes" {
+	            $script:testY | should benullorempty
+	        }
+	  			It "executes -No" {
+	            $script:testN | should be $true
+	        }
+	        It 'passes item as $_ to -No' {
+	        		$script:testNValue | should be $testDir
+	        }
+	        It "executes -Always" {
+	            $script:testA | should be $true
+	        }
+	        It 'passes item as $_ to -Always' {
+							$script:testAValue | should be $testDir
+	        }
+        }
+    }
---
+++ Exists-Item/Exists-Item.ps1
@@ -0,0 +1,24 @@
+function Exists-Item {
+    param(
+        [Parameter(Mandatory=$true, Position=1)]
+        $item,
+        [Parameter(Mandatory=$false, Position=2)]
+        $Yes,
+        [Parameter(Mandatory=$false, Position=3)]
+        $No,
+        [Parameter(Mandatory=$false, Position=4)]
+        $Always
+    )
+    if ((Get-Item $item -ea si) -ne $null) {
+        if ($Yes -ne $null) {
+            $item | &{ PROCESS { &$Yes } }
+        }
+    } else {
+        if ($No -ne $null) {
+            $item | &{ PROCESS { &$No } }
+        }
+    }
+    if ($Always -ne $null) {
+        $item | &{ PROCESS { &$Always } }
+    }
+}

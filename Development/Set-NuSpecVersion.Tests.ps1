39b369a8bac2087b70647cd2c1842ec9284e458a
Tim Rayburn <tim@timrayburn.net>
2013-05-05 17:03:32 -0500

    Set-NuSpecVersion

---
+++ Development/Set-NuSpecVersion.Tests.ps1
@@ -0,0 +1,26 @@
+$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+    $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.", ".")
+    . "$here\$sut"
+
+    Describe "Set-NuSpecVersion" -tags "wip" {
+
+        $testFile = "$here\..\TestData\Example.nuspec"
+
+        Context "When nuspec exists" {
+
+            $nuspec = "TestDrive:\Example.nuspec"
+
+            if (Test-Path $nuspec) {
+                Remove-Item "TestDrive:\Example.nuspec"
+            }
+
+            copy $testFile $nuspec
+
+            Set-NuSpecVersion $nuspec 1.0.0-Beta9
+            $xml = [xml] (Get-Content $nuspec)
+
+            It "sets version to a new value" {
+                $xml.package.metadata.version | should be "1.0.0-Beta9"
+            }
+        }
+    }
---
+++ Development/Set-NuSpecVersion.ps1
@@ -0,0 +1,10 @@
+function Set-NuSpecVersion {
+    param(
+        [Parameter(Mandatory=$true)] [string] $Path,
+        [Parameter(Mandatory=$true)] [string] $Version
+    )
+
+    $xml = [xml] (Get-Content $Path)
+    $xml.package.metadata.version = $Version
+    $xml.Save((Get-Item $Path).FullName)
+}

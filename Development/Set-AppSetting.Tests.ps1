a45aa2989f4c9d14cb373e89ebe16c7f7cf4094e
Tim Rayburn <tim@timrayburn.net>
2013-05-04 23:46:40 -0500

    Set-AppSetting created

---
+++ Development/Set-AppSetting.Tests.ps1
@@ -0,0 +1,69 @@
+$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+    $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.", ".")
+    . "$here\$sut"
+
+    Describe "Set-AppSetting" -tags "wip" {
+        $config = "TestDrive:\Web.config"
+        $results = @{}
+
+        Context "When AppSetting already exists" {
+            
+            Copy-Item "$here\..\TestData\Web.config" $config
+            Set-AppSetting $config "ClientValidationEnabled" 123456
+            $xml = [xml] (Get-Content $config)
+
+            It "Should have changed setting value" {
+                $xml.configuration.appSettings.add | ? { 
+                    $_.key -eq "ClientValidationEnabled" } | % {
+                    $_.value | should not benullorempty
+                    $_.value | should be 123456 }
+            }
+
+            It "Should not have changed other settings" {
+                $xml.configuration.appSettings.add | ? { 
+                    $_.key -eq "UnobtrusiveJavaScriptEnabled" } | % {
+                    $_.value | should not benullorempty
+                    $_.value | should be "true" }
+
+                $xml.configuration.appSettings.add | ? { 
+                    $_.key -eq "webpages:Version" } | % {
+                    $_.value | should not benullorempty
+                    $_.value | should be "1.0.0.0" }
+            }
+        }
+
+        Context "When AppSetting does not exist" {
+
+            Copy-Item "$here\..\TestData\Web.config" $config
+            Set-AppSetting $config "BrandNewSetting" 123456
+            $xml = [xml] (Get-Content $config)
+
+            It "Should create the new setting" {
+                $xml.configuration.appSettings.add.Count | should be 4
+            }
+
+            It "Should have changed setting value" {
+                $xml.configuration.appSettings.add | ? { 
+                    $_.key -eq "BrandNewSetting" } | % {
+                    $_.value | should not benullorempty
+                    $_.value | should be 123456 }
+            }
+
+            It "Should not have changed other settings" {
+                $xml.configuration.appSettings.add | ? { 
+                    $_.key -eq "ClientValidationEnabled" } | % {
+                    $_.value | should not benullorempty
+                    $_.value | should be "true" }
+
+                $xml.configuration.appSettings.add | ? { 
+                    $_.key -eq "UnobtrusiveJavaScriptEnabled" } | % {
+                    $_.value | should not benullorempty
+                    $_.value | should be "true" }
+
+                $xml.configuration.appSettings.add | ? { 
+                    $_.key -eq "webpages:Version" } | % {
+                    $_.value | should not benullorempty
+                    $_.value | should be "1.0.0.0" }
+            }
+        }
+    }
---
+++ Development/Set-AppSetting.ps1
@@ -0,0 +1,25 @@
+function Set-AppSetting {
+    param(
+        [Parameter(Mandatory=$true)] [string] $Path,
+        [Parameter(Mandatory=$true)] [string] $Key,
+        [Parameter(Mandatory=$true)] [string] $Value
+    )
+
+    $xml = ([xml](Get-Content $Path))
+    $foundSettings = $xml.configuration.appSettings.add | ? { $_.key -eq $Key } 
+    if ($foundSettings.Count -ne 0) {
+        $foundSettings | % { $_.value = $Value } }
+    else {
+        $el = $xml.CreateElement("add")
+        $kat = $xml.CreateAttribute("key")
+        $kat.Value = $Key
+        $vat = $xml.CreateAttribute("value")
+        $vat.Value = $Value
+    
+        $el.SetAttributeNode($kat) | Out-Null
+        $el.SetAttributeNode($vat) | Out-Null
+    
+        $xml.configuration.appSettings.AppendChild($el) | Out-Null
+    }
+    Set-Content -Path $Path -Value $xml.OuterXml
+}

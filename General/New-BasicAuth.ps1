029e2faea5e22efc727b5f8a2de78c31b7fc4bc3
Tim Rayburn <tim@timrayburn.net>
2013-05-07 18:01:14 -0500

    Exporting module member for New-BasicAuth

--- General/New-BasicAuth.ps1
+++ General/New-BasicAuth.ps1
@@ -3,3 +3,5 @@ function New-BasicAuth {
     $auth64 = [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($user+":"+$pass ))
     return @{ Authorization="Basic $auth64" }
 }
+
+Export-ModuleMember New-BasicAuth

3b638b51ac280cfcb0b8d6846fb3a9684be86780
Tim Rayburn <tim@timrayburn.net>
2013-05-21 09:52:05 -0500

    Get-AppSetting

---
+++ Development/Get-AppSetting.Tests.ps1
@@ -0,0 +1,45 @@
+$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+    $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.", ".")
+    . "$here\$sut"
+
+    Describe "Get-AppSetting" -tag "WIP" {
+
+        Context "When file exists and setting exists" {
+            $config = "TestDrive:\Web.config"
+            Copy-Item "$here\..\TestData\Web.config" $config
+            $val = Get-AppSetting $config "ClientValidationEnabled"
+
+            It "should return the value if present" {
+                $val | should not benullorempty
+                $val | should be "true"
+            }
+        }
+
+        Context "When the files does not exist" {
+            $config = "TestDrive:\Web.config"
+            Remove-Item $config -ErrorAction SilentlyContinue
+            $ex = ""
+
+            try {
+                $val = Get-AppSetting $config "ClientValidationEnabled"
+            }
+            catch [Exception] {
+                $ex = $_.Exception
+            }
+
+            It "should throw a FileNotFoundException" {
+                $ex.GetType().Name | should be "FileNotFoundException"
+            }
+        }
+
+        Context "When file exists and setting exists" {
+            $config = "TestDrive:\Web.config"
+            Copy-Item "$here\..\TestData\Web.config" $config
+            $val = Get-AppSetting $config "SettingThatDoesNotExist"
+
+            It "should return null" {
+                $val | should benullorempty
+            }
+        }
+
+    }
---
+++ Development/Get-AppSetting.ps1
@@ -0,0 +1,16 @@
+function Get-AppSetting {
+    param(
+        [string] $path,
+        [string] $key
+    )
+
+    if (Test-Path $path) {
+        $config = [xml](Get-Content $path)
+        $settings = $config.configuration.appSettings.add
+        return $settings | ? { $_.key -eq $key } | Select-Object -First 1 | %{ $_.value }
+    } else {
+        throw (New-Object "System.IO.FileNotFoundException" $path)
+    }
+}
+
+Export-ModuleMember Get-AppSetting

573c760768701357b58fdc3b4fdae31d493ce81a
Tim Rayburn <tim@timrayburn.net>
2013-06-12 14:06:32 -0500

    trying VS2012 and non-admin

--- BasicSetup.ps1
+++ BasicSetup.ps1
@@ -5,6 +5,62 @@ function Execute([string] $cmd)
         -ExecutionPolicy unrestricted `
         -Command $cmd
 }
+
+Function Test-RegistryValue 
+{
+    param(
+        [Alias("RegistryPath")]
+        [Parameter(Position = 0)]
+        [String]$Path
+        ,
+        [Alias("KeyName")]
+        [Parameter(Position = 1)]
+        [String]$Name
+    )
+
+    process 
+    {
+        if (Test-Path $Path) 
+        {
+            $Key = Get-Item -LiteralPath $Path
+            if ($Key.GetValue($Name, $null) -ne $null)
+            {
+                if ($PassThru)
+                {
+                    Get-ItemProperty $Path $Name
+                }       
+                else
+                {
+                    $true
+                }
+            }
+            else
+            {
+                $false
+            }
+        }
+        else
+        {
+            $false
+        }
+    }
+}
+
+Function Disable-UAC
+{
+    $EnableUACRegistryPath = "REGISTRY::HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\System"
+    $EnableUACRegistryKeyName = "EnableLUA"
+    $UACKeyExists = Test-RegistryValue -RegistryPath $EnableUACRegistryPath -KeyName $EnableUACRegistryKeyName 
+    if ($UACKeyExists)
+    {
+        Set-ItemProperty -Path $EnableUACRegistryPath -Name $EnableUACRegistryKeyName -Value 0
+    }
+    else
+    {
+        New-ItemProperty -Path $EnableUACRegistryPath -Name $EnableUACRegistryKeyName -Value 0 -PropertyType "DWord"
+    }
+}
+
 # Install Chocolatey
 Execute "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))"
 sc Env:\Path "$(gc Env:\Path);$(gc Env:\SystemDrive)\chocolatey\bin"
@@ -22,7 +78,7 @@ cwindowsfeatures NetFx3
 # Chocolatey Packages
 cinst TimRayburn.GitAliases
 cinst ruby
-# cinst dropbox # Problem with running as Admin, skip for now
+cinst dropbox # Problem with running as Admin, skip for now
 cinst paint.net
 cinst beyondcompare
 cinst 7zip
@@ -41,3 +97,10 @@ cWindowsFeatures TelnetClient
 cWindowsFeatures WCF-Services45
 cWindowsFeatures WCF-TCP-PortSharing45
 cwindowsfeatures IIS-WebServerManagementTools
+
+# Add Visual Studio 2012
+cinst VisualStudio2012Ultimate
+
+# Disable UAC & Reboot
+# Disable-UAC
+# Restart-Computer -Force -Confirm:$false

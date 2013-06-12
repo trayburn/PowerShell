3b58daf786d006c9f6e976923acffe8d5c3e9292
Tim Rayburn <tim@timrayburn.net>
2013-06-12 13:14:17 -0500

    Working on development environment setup

---
+++ BasicSetup.ps1
@@ -0,0 +1,16 @@
+function Execute([string] $cmd)
+{
+    powershell `
+        -NoProfile `
+        -ExecutionPolicy unrestricted `
+        -Command $cmd
+}
+# Install Chocolatey
+Execute "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))"
+sc Env:\Path "$(gc Env:\Path);$(gc Env:\SystemDrive)\chocolatey\bin"
+
+# Install PSGet
+Execute "(new-object Net.WebClient).DownloadString('http://psget.net/GetPsGet.ps1') | iex"
+
+# Get PowerShell modules
+Install-Module Pester
